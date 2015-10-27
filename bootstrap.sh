9a50dce94d8f9207961d4ba28964c3cd8e144df6
Sara <sara.kau@hotmail.com>
2015-10-27 20:25:56 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,11 @@ source config/environment.sh
 echo "Luodaan projektikansio..."
 
 # Luodaan projektin kansio
-ssh $USERNAME@users.cs.helsinki.fi "
+ssh kauvo@users.cs.helsinki.fi "
 cd htdocs
 touch favicon.ico
-mkdir $PROJECT_FOLDER
-cd $PROJECT_FOLDER
+mkdir muistilista
+cd muistilista
 touch .htaccess
 echo 'RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-f
@@ -21,18 +21,18 @@ echo "Valmis!"
 echo "Siirretään tiedostot users-palvelimelle..."
 
 # Siirretään tiedostot palvelimelle
-scp -r app config lib vendor sql assets index.php composer.json $USERNAME@users.cs.helsinki.fi:htdocs/$PROJECT_FOLDER
+scp -r app config lib vendor sql assets index.php composer.json kauvo@users.cs.helsinki.fi:htdocs/muistilista
 
 echo "Valmis!"
 
 echo "Asetetaan käyttöoikeudet ja asennetaan Composer..."
 
 # Asetetaan oikeudet ja asennetaan Composer
-ssh $USERNAME@users.cs.helsinki.fi "
+ssh kauvo@users.cs.helsinki.fi "
 chmod -R a+rX htdocs
-cd htdocs/$PROJECT_FOLDER
+cd htdocs/muistilista
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
 exit"
 
-echo "Valmis! Sovelluksesi on nyt valmiina osoitteessa $USERNAME.users.cs.helsinki.fi/$PROJECT_FOLDER"
+echo "Valmis! Sovelluksesi on nyt valmiina osoitteessa kauvo.users.cs.helsinki.fi/muistilista"
